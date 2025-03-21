90602b19ff316e9aef7fb4c82d9d1983cd8a0cfb
Momitul Hoque Chowdhury <110973021+Momit87@users.noreply.github.com>
2025-03-21 15:48:35 +0600

    Add files via upload

---
+++ car_dealership/users/models.py
@@ -0,0 +1,6 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    is_dealership_staff = models.BooleanField(default=False)
